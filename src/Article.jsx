a955b90f28b86ebda0083199ada56285e1d3a235
Gero Gerke <11deutron11@gmail.com>
2018-06-08 14:24:22 +0200

    Add Publisher Logos to Display

--- src/Article.jsx
+++ src/Article.jsx
@@ -7,11 +7,16 @@ import './Article.css';
 class Article extends React.Component {
     render() {
         return (
-            <div className="Article">
-                <img src="https://s2.reutersmedia.net/resources/r/?m=02&d=20180608&t=2&i=1270372000&w=1200&r=LYNXNPEE570NG"/>
-                <div className="Article-text">
-                    <span className="Article-text_title">{this.props.flag} {this.props.title}</span>
-                    <span className="Article-text_headline">{this.props.headline}</span>
+            <div className='Article'>
+                <img className='Article-thumb' src='https://s2.reutersmedia.net/resources/r/?m=02&d=20180608&t=2&i=1270372000&w=1200&r=LYNXNPEE570NG'/>
+                <div className='Article-text'>
+                    <span className='Article-text_title'>{this.props.flag} {this.props.title}</span>
+                    <span className='Article-text_headline'>{this.props.headline}</span>
+                    <div className='row'>
+                        <img className='logo' src={this.props.logo}/>
+                        <span style={{ marginRight: '5px' }}>&#9679;</span>
+                        {this.props.tags.map((tag, i) => (<span key={i} className='Article-text_tag'>{tag}</span>))}
+                    </div>
                 </div>
             </div>
         );
@@ -21,7 +26,9 @@ class Article extends React.Component {
 Article.propTypes = {
     flag: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    headline: PropTypes.string.isRequired
+    headline: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
+    logo: PropTypes.string.isRequired
 };
 
 export default Article;
